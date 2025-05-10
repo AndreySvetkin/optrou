63d947940c36654cbcf40abe2e1ab7c1a738cf6d
Reedemir <andreysvetkin711@gmail.com>
2025-05-10 15:24:10 +0400

    delete fuel tank

--- src/main/java/com/svetkin/optrou/entity/Vehicle.java
+++ src/main/java/com/svetkin/optrou/entity/Vehicle.java
@@ -2,22 +2,17 @@ package com.svetkin.optrou.entity;
 
 import com.svetkin.optrou.entity.type.DriverLicenseCategory;
 import com.svetkin.optrou.entity.type.FuelType;
-import io.jmix.core.DeletePolicy;
 import io.jmix.core.MetadataTools;
 import io.jmix.core.annotation.DeletedBy;
 import io.jmix.core.annotation.DeletedDate;
 import io.jmix.core.entity.annotation.JmixGeneratedValue;
-import io.jmix.core.entity.annotation.OnDelete;
-import io.jmix.core.metamodel.annotation.Composition;
 import io.jmix.core.metamodel.annotation.DependsOnProperties;
 import io.jmix.core.metamodel.annotation.InstanceName;
 import io.jmix.core.metamodel.annotation.JmixEntity;
-import io.jmix.core.metamodel.annotation.JmixProperty;
 import io.jmix.maps.utils.GeometryUtils;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
 import jakarta.validation.constraints.NotNull;
@@ -26,7 +21,6 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @JmixEntity
@@ -58,6 +52,9 @@ public class Vehicle {
     @Column(name = "DELETED_DATE")
     private OffsetDateTime deletedDate;
 
+    @Column(name = "VEHICLE_ID")
+    private String vehicleId;
+
     @Column(name = "LICENSE_PLATE", nullable = false)
     @NotNull
     private String licensePlate;
@@ -74,13 +71,16 @@ public class Vehicle {
     @NotNull
     private Integer fuelType;
 
-    @Column(name = "FUEL_CONSUMPTION")
+    @NotNull
+    @Column(name = "FUEL_CONSUMPTION", nullable = false)
     private Double fuelConsumption;
 
-    @OnDelete(DeletePolicy.CASCADE)
-    @Composition
-    @OneToMany(mappedBy = "vehicle")
-    private List<FuelTank> tanks;
+    @NotNull
+    @Column(name = "CAPACITY", nullable = false)
+    private Double capacity;
+
+    @Column(name = "REMAINING_FUEL")
+    private Double remainingFuel;
 
     @Column(name = "LOCATION")
     private Point location;
@@ -91,20 +91,28 @@ public class Vehicle {
     @Column(name = "LONGITUDE")
     private Double longitude;
 
-    @DependsOnProperties({"tanks"})
-    @JmixProperty
-    public Double getRemainingFuel() {
-        return tanks.stream()
-                .mapToDouble(FuelTank::getRemainingFuel)
-                .sum();
+    public String getVehicleId() {
+        return vehicleId;
     }
 
-    @DependsOnProperties({"tanks"})
-    @JmixProperty
+    public void setVehicleId(String vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public void setCapacity(Double capacity) {
+        this.capacity = capacity;
+    }
+
     public Double getCapacity() {
-        return tanks.stream()
-                .mapToDouble(FuelTank::getCapacity)
-                .sum();
+        return capacity;
+    }
+
+    public void setRemainingFuel(Double remainingFuel) {
+        this.remainingFuel = remainingFuel;
+    }
+
+    public Double getRemainingFuel() {
+        return remainingFuel;
     }
 
     public String getModel() {
@@ -113,14 +121,6 @@ public class Vehicle {
 
     public void setModel(String model) {
         this.model = model;
-    }
-
-    public List<FuelTank> getTanks() {
-        return tanks;
-    }
-
-    public void setTanks(List<FuelTank> tanks) {
-        this.tanks = tanks;
     }
 
     public Double getFuelConsumption() {
